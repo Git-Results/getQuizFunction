4924e603b8e6a5b5d125cac4e1e5d770bb35651d
Alishia Patricio <alishia.m.patricio@gmail.com>
2022-11-09 11:17:28 -0500

    update message

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,8 @@ def lambda_handler(event, context):
   items=response['Items']
   print(items)
   
+  #This is an update to the lambda
+  
   return {
     'isBase64Encoded': False,
     'headers': { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'http://my-static-bucket-jenkins.s3-website-us-east-1.amazonaws.com' },
