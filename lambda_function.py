93be7ef960c2d14c9a5e5f9618f5a76b44d0ef22
ampatricio <81889689+ampatricio@users.noreply.github.com>
2022-11-08 22:20:13 -0500

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+#Update 1
+def lambda_handler(event, context):
+  client = boto3.resource('dynamodb', region_name='us-east-1')
+  table = client.Table('gitCCP_quiz')   
+  response =table.scan()
+  items=response['Items']
+  print(items)
+  
+  return {
+    'isBase64Encoded': False,
+    'headers': { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'http://my-static-bucket-jenkins.s3-website-us-east-1.amazonaws.com' },
+    'statusCode': 200,
+    'body': json.dumps(items)
+  }
